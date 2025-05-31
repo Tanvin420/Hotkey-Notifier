3a26dbd0d859b2d6313a43355097e331a2520759
Tanvin420 <tanvinmdjamaluddintanvin@hotmail.com>
2025-05-31 21:38:18 +0600

    Windows Installer 1.1

--- setup_build.py
+++ setup_build.py
@@ -1,15 +1,24 @@
 from cx_Freeze import setup, Executable
 
-# Dependencies
+# Build options
 build_exe_options = {
-    "packages": ["tkinter", "pystray", "keyboard", "pyperclip", "os", "sys","time","threading","PIL","tempfile","json","winreg","webbrowser"],
-    "include_files": ["notifier_config.json","app_icon.ico","LICENSE"]
+    "packages": ["tkinter", "pystray", "keyboard", "pyperclip", "os", "sys", "time", "threading", "PIL", "tempfile", "json", "winreg", "webbrowser"],
+    "include_files": ["notifier_config.json", "app_icon.ico", "LICENSE"]
+}
+
+bdist_msi_options = {
+    "upgrade_code": "{a1f1e6f2-6c52-451b-b70d-0705d42ae234}",  
+    "add_to_path": False,
+    "initial_target_dir": r"[ProgramFilesFolder]\HotkeyNotifier"
 }
 
 setup(
     name="Hotkey Notifier",
     version="1.1",
     description="Custom popup notifier for hotkeys",
-    options={"build_exe": build_exe_options},
+    options={
+        "build_exe": build_exe_options,
+        "bdist_msi": bdist_msi_options
+    },
     executables=[Executable("hot_key.py", base="Win32GUI", icon="app_icon.ico")]
 )
